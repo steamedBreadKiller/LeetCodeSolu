f6788290cf6698bbc71d0f328d0b39ee14c8722f
wangjingbiao <577322753@qq.com>
2020-05-07 21:27:28 +0800

    提交最长不重复字符串题解

--- src/main/java/top/learningwang/str/LongSonStr.java
+++ src/main/java/top/learningwang/str/LongSonStr.java
@@ -1,5 +1,8 @@
 package top.learningwang.str;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author wangjingbiao
  * createTime: 2020/5/7 下午6:02
@@ -7,24 +10,35 @@ package top.learningwang.str;
  */
 public class LongSonStr {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(solution.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(solution.lengthOfLongestSubstring(" "));
     }
 
     static class Solution {
         public int lengthOfLongestSubstring(String s) {
-            long maxLen = 0;
+            if (s == null || "".equals(s)) {
+                return 0;
+            }
+            Set<Character> hashSet = new HashSet<Character>();
+            int maxLen = 1;
             for (int i = 0; i < s.length() - 1; i++) {
-                long currentMaxLen = 1;
+                int currentMaxLen = 1;
                 char currentChar = s.charAt(i);
+                hashSet.add(currentChar);
                 for (int j = i + 1; j < s.length(); j++) {
-                    if(s.charAt(j)!=currentChar){
-                        currentMaxLen++;
-                    }else{
+                    if (hashSet.contains(s.charAt(j))) {
+                        hashSet.clear();
                         break;
                     }
+                    hashSet.add(s.charAt(j));
+                    currentMaxLen++;
                 }
+                maxLen = Math.max(maxLen, currentMaxLen);
             }
-            return 0;
+            return maxLen;
         }
     }
 }
